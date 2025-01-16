a9273fc07111c204e45be207d6cbea9a7f148e30
amrks7800 <amr.khaled_a646@fsh.kfs.edu.eg>
2025-01-16 11:35:12 +0200

    initial

---
+++ src/content/config.ts
@@ -0,0 +1,26 @@
+import { defineCollection, z } from 'astro:content'
+
+const postsCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    published: z.date(),
+    updated: z.date().optional(),
+    draft: z.boolean().optional().default(false),
+    description: z.string().optional().default(''),
+    image: z.string().optional().default(''),
+    tags: z.array(z.string()).optional().default([]),
+    category: z.string().optional().default(''),
+    lang: z.string().optional().default(''),
+    author: z.string().optional().default('Amr khaled'),
+    cover: z.string().optional().default("https://images.unsplash.com/photo-1572283046480-e990be92d301?w=1960&h=1102&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fGJsYWNrfGVufDB8MHwwfHx8Mg%3D%3D"),
+
+    /* For internal use */
+    prevTitle: z.string().default(''),
+    prevSlug: z.string().default(''),
+    nextTitle: z.string().default(''),
+    nextSlug: z.string().default(''),
+  }),
+})
+export const collections = {
+  posts: postsCollection,
+}
---
+++ src/content/spec/about.md
@@ -0,0 +1,9 @@
+# About
+This is the personal blog for [Amr khaled](https://github.com/amrks7800).
+
+::github{repo="amrks7800/personal-blog"}
+
+> ### Sources of images used in this site
+> - [Unsplash](https://unsplash.com/)
+> - [星と少女](https://www.pixiv.net/artworks/108916539) by [Stella](https://www.pixiv.net/users/93273965)
+> - [Rabbit - v1.4 Showcase](https://civitai.com/posts/586908) by [Rabbit_YourMajesty](https://civitai.com/user/Rabbit_YourMajesty)
